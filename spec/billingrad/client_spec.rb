70b755b8cbd62a013567d3b5d6371a33aaa0b8a8
Pavel Bozhenik <bozhenik@gmail.com>
2014-12-25 19:51:22 +0400

    Add spec's

---
+++ spec/billingrad/client_spec.rb
@@ -0,0 +1,43 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe Billingrad::Client do
+
+  context '.new' do
+
+    before do
+      @client = Billingrad::Client.new open: 'open-key', close: 'close-key', did: 'delivery-id'
+    end
+
+    describe '.test' do
+      it 'should return an array with correct test data' do
+        expect(@client.test['data']['sum']).to eq(3)
+      end
+    end
+
+    describe '.projects' do
+      it 'should return an array with correct project data' do
+        expect(@client.projects['data'][0]['id']).to eq('df554239ebdc00553963a9ee7d64b406')
+      end
+    end
+
+    describe '.deliveries' do
+      it 'should return an array with correct delivery data' do
+        expect(@client.deliveries(pid: 'project-id')['data'][2]['did']).to eq(90)
+      end
+    end
+
+    describe '.send' do
+      it 'should return an array with correct message id' do
+        expect(@client.send(to: 'phone-number', text: 'test message')['data']).to eq(6008.to_s)
+      end
+    end
+
+    describe '.info' do
+      it 'should return an array with correct message info data' do
+        expect(@client.info(mid: 'message-id')['data']['deliveryCount']).to eq(1)
+      end
+    end
+
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'webmock/rspec'
+require File.expand_path('../support/fake_billingrad', __FILE__)
+require File.expand_path('../../lib/billingrad', __FILE__)
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:any, /my.billingrad.com/).to_rack(FakeBillingrad)
+  end
+end
